16cbd18727576a6e3e9c8c8b9f6865a7bcd203ca
Miqayel <miqayel@e-works.am>
2020-06-14 22:37:17 +0400

    changes

--- src/components/pie-chart/index.js
+++ src/components/pie-chart/index.js
@@ -4,13 +4,7 @@ import CreateFiled from './create-field';
 
 const PieChart = () => {
     return (
-<<<<<<< HEAD
         <CreateFiled />
-=======
-        <>
-            <CreateFiled />
-        </>
->>>>>>> 619efbd1eeac9705bc48278b2141fdd15c3ec757
     );
 };
 
--- src/redux-implementation/index.js
+++ src/redux-implementation/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
-<<<<<<< HEAD
 import { END } from 'redux-saga';
 import { fromJS } from 'immutable';
 import logger from 'redux-logger';
@@ -9,19 +8,6 @@ import history from '../session-history';
 import reducer from './reducer';
 
 function getStore() {
-=======
-import createSagaMiddleware, { END } from 'redux-saga';
-import { fromJS } from 'immutable';
-import logger from 'redux-logger';
-
-import history from 'session-history';
-import reducer from './reducer';
-import rootSaga from './saga';
-
-function getStore() {
-    const sagaMiddleware = createSagaMiddleware();
-
->>>>>>> 619efbd1eeac9705bc48278b2141fdd15c3ec757
     let initialState = {};
     if (typeof window != 'undefined') {
         for (let item in window.__INITIAL_STATE__) {
@@ -30,20 +16,11 @@ function getStore() {
     }
 
     const enhancer = (process.env && process.env.NODE_ENV) !== 'production'
-<<<<<<< HEAD
         ? applyMiddleware(routerMiddleware(history), logger) : applyMiddleware(routerMiddleware(history));
 
     const store = typeof window != 'undefined' ? createStore(reducer, initialState, enhancer) : createStore(reducer, enhancer);
     if (process.env && process.env.NODE_ENV !== 'production' && typeof window != 'undefined') window.store = store;
 
-=======
-        ? applyMiddleware(sagaMiddleware, routerMiddleware(history), logger) : applyMiddleware(sagaMiddleware, routerMiddleware(history));
-
-    const store: any = typeof window != 'undefined' ? createStore(reducer, initialState, enhancer) : createStore(reducer, enhancer);
-    if (process.env && process.env.NODE_ENV !== 'production' && typeof window != 'undefined') window.store = store;
-
-    sagaMiddleware.run(rootSaga);
->>>>>>> 619efbd1eeac9705bc48278b2141fdd15c3ec757
     store.close = () => store.dispatch(END);
 
     return store;
